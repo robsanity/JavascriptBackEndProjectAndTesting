d811be0cd40b8f343ab2af413c6ba87adcb0fc76
Lorenzo <48474246+lorenzobuompane@users.noreply.github.com>
2022-05-14 12:34:01 +0200

    add skuitems query

--- code/server/modules/SKUItemsDAO.js
+++ code/server/modules/SKUItemsDAO.js
@@ -2,38 +2,84 @@
 
 function listSKUItems() {
     return new Promise((resolve, reject) => {
-
+        const sql = "SELECT * FROM SKUItems";
+        db.all(sql, [], (err, rows) => {
+            if (err) {
+                reject({ error: "no SKUItems in database" });
+                return;
+            }
+            const SKUItems = rows.map((t) => ({ RFID: t.RFID, SKUId: t.idSKU, Available: t.available, DateOfStock: t.dateOfStock }));
+            resolve(SKUItems);
+        });
     });
 };
 
-function findSKUItems() {
+function findSKUItems(SKUId) {
     return new Promise((resolve, reject) => {
-
+        const sql = "SELECT * FROM SKUItems WHERE available=1 AND idSKU=?";
+        db.all(sql, [SKUId], (err, rows) => {
+            if (err) {
+                reject({ error: "no SKUItems in database" });
+                return;
+            }
+            const SKUItems = rows.map((t) => ({ RFID: t.RFID, SKUId: t.idSKU, DateOfStock: t.dateOfStock }));
+            resolve(SKUItems);
+        });
     });
 };
 
-function findSKUItem() {
+function findSKUItem(rfid) {
     return new Promise((resolve, reject) => {
-
+        const sql = "SELECT * FROM SKUItems WHERE RFID=?";
+        db.all(sql, [rfid], (err, rows) => {
+            if (err) {
+                reject({ error: "no SKUItems in database" });
+                return;
+            }
+            const SKUItems = rows.map((t) => ({ RFID: t.RFID, SKUId: t.idSKU, Available: t.available, DateOfStock: t.dateOfStock }));
+            resolve(SKUItems);
+        });
     });
 };
 
-function createSKUItem() {
+function createSKUItem(rfid, SKUId, date) {
     return new Promise((resolve, reject) => {
+        const sql = "INSERT INTO SKUItems (RFID, idSKU, available, dateOfStock) values (?,?,?,?)";
+        db.all(sql, [rfid, SKUId, 0, date], (err, rows) => {
+            if (err) {
+                reject({ error: "no insert" });
 
+            }
+            resolve(true);
+        });
     });
 };
 
-function modifySKUItem() {
+
+function modifySKUItem(rfid, newrfid, newavailable, newdate) {
     return new Promise((resolve, reject) => {
+        const sql = "UPDATE SKUItems SET RFID=?, available=?, dateOfStock=? WHERE RFID=?";
+        db.all(sql, [newrfid, newavailable, newdate, rfid], (err, rows) => {
+            if (err) {
+                reject({ error: "no update" });
 
+            }
+            resolve(true);
+        });
     });
 };
 
-function deleteSKUItem() {
+function deleteSKUItem(rfid) {
     return new Promise((resolve, reject) => {
+        const sql = "DELETE FROM SKUItems WHERE RFID=?";
+        db.all(sql, [rfid], (err, rows) => {
+            if (err) {
+                reject({ error: "no delete" });
 
+            }
+            resolve(true);
+        });
     });
 };
 
-module.exports = { listSKUItems, findSKUItems, findSKUItem, createSKUItem, modifySKUItem, deleteSKUItem }
+module.exports = { listSKUItems, findSKUItems, findSKUItem, checkSKUItems, createSKUItem, modifySKUItem, deleteSKUItem }
