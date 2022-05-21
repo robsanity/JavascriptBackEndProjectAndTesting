8c67d275f777198dcfb60dd46725f61f311656af
ROBERTO DI CIAULA S299444 <s299444@studenti.polito.it>
2022-05-21 14:48:29 +0200

    we

--- code/server/unit_test/SKU.test.js
+++ code/server/unit_test/SKU.test.js
@@ -1,10 +1,55 @@
-const { expect } = require('chai');
-const SKUsDAO = require('./modules/SKUsDAO');
+const SKUsDAO = require('../modules/SKUsDAO');
+const db = require ('../db.js');
 
 
 
-function testSKU(){
-    test('test SKU', () =>{
-        expect()
-    });
-}
+describe("Test SKUs", () => {
+    beforeAll(() => {
+        SKUsDAO.deleteDatas();
+    })
+
+    beforeEach(() => SKUsDAO.deleteDatas())
+
+    test("Database start", async () => {
+        let res = await SKUsDAO.listSKUs();
+        expect(res.length).toStrictEqual(0);
+    })
+    
+    testlistSKUs();
+})
+
+
+function testlistSKUs(){
+    test("Test Get All SKU ", async () => {
+        let res = await SKUsDAO.listSKUs();
+        expect(res.length).toStrictEqual(0);
+    
+        let newSKU = await SKUsDAO.createSKU(
+          "FRANCO",
+          12,
+          20,
+          'ciccio',
+          15,
+          10.45      
+        );
+    
+        res = await SKUsDAO.listSKUs();
+        expect(res.length).toStrictEqual(1);
+    
+        newSKU = await SKUsDAO.createSKU(
+          'MAMT',
+          10,
+          15,
+          'lello',
+          11,
+          22.30
+
+        );
+    
+        res = await SKUsDAO.listSKUs();
+        expect(res.length).toStrictEqual(2);
+      });
+    }
+  function testUpdateSKU(){
+    test('update ')
+  }
