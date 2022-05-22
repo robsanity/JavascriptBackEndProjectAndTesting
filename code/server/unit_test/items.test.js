4d2369faf33e8677cb19b0632a7331c3effb193e
Simone Romantini s304907 <s304907@studenti.polito.it>
2022-05-22 15:05:44 +0200

    test items

--- code/server/unit_test/items.test.js
+++ code/server/unit_test/items.test.js
@@ -18,9 +18,8 @@ describe("Test item", () => {
 
     testGetItems();
     testFindItem(1);
-    /*testInsertItems(description, id, SKUId, supplierId, price);
-    testUpdateItem(description, id, SKUId, supplierId, price)
-    testDeleteItem();*/
+    testUpdateItem(1, "new description", 99)
+    testDeleteItem(1);
 })
 
 function testGetItems() {
@@ -65,17 +64,67 @@ function testFindItem(id) {
             2,
             10.99
         );
-        console.log(newItem.description); //stampa undefined
         expect(newItem).toStrictEqual(true);
         res = await itemsDAO.listItems();
         expect(res.length).toStrictEqual(1);
 
         let findItem = await itemsDAO.findItem(id);
-            console.log(findItem.description); //stampa undefined
         expect(findItem[0].description).toStrictEqual("a new item");
         expect(findItem[0].id).toStrictEqual(id);
         expect(findItem[0].SKUId).toStrictEqual(1);
         expect(findItem[0].suppliersId).toStrictEqual(2);
         expect(findItem[0].price).toStrictEqual(10.99);
+    })
+}
+
+function testUpdateItem(id, newDescription, newPrice) {
+    test("updateUserItem", async () => {
+        let res = await itemsDAO.listItems();
+        expect(res.length).toStrictEqual(0);
+
+        let newItem = await itemsDAO.createItem(
+            "a new item",
+            id,
+            1,
+            2,
+            10.99
+        );
+        expect(newItem).toStrictEqual(true);
+        res = await itemsDAO.listItems();
+        expect(res.length).toStrictEqual(1);
+
+        res = await itemsDAO.updateItem(id, newDescription, newPrice);
+        expect(res).toStrictEqual(true);
+
+        let findItem = await itemsDAO.findItem(id);
+
+        expect(findItem[0].description).toStrictEqual(newDescription);
+        expect(findItem[0].id).toStrictEqual(id);
+        expect(findItem[0].price).toStrictEqual(newPrice);
+    })
+}
+
+function testDeleteItem(id) {
+    test("deleteItem", async () => {
+        let res = await itemsDAO.listItems();
+        expect(res.length).toStrictEqual(0);
+
+        let newItem = await itemsDAO.createItem(
+            "a new item",
+            id,
+            1,
+            2,
+            10.99
+        );
+        expect(newItem).toStrictEqual(true);
+        
+        res = await itemsDAO.listItems();
+        expect(res.length).toStrictEqual(1);
+
+        res = await itemsDAO.deleteItem(id);
+        expect(res).toStrictEqual(true);
+
+        res = await itemsDAO.listItems();
+        expect(res.length).toStrictEqual(0);
     })
 }
